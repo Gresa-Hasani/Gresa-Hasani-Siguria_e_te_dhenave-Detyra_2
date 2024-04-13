8d749c374a97a7c958c0b4afa9d7ba01d2949e84
gr3sa-h1 <154631164+gr3sa-h1@users.noreply.github.com>
2024-04-13 13:13:58 +0200

    updated ColumnarTransposition

--- DoubleTransposition/ColumnarTransposition.java
+++ DoubleTransposition/ColumnarTransposition.java
@@ -39,3 +39,42 @@ public class ColumnarTransposition {
         return ciphertext.toString();
     }
 }
+
+public static String decrypt(String cipherText, String keywordCols) {
+        String decryptedText = "";
+        int gjatesiaKeyword = keywordCols.length();
+        int gjatesiaCipherText = cipherText.length();
+        int rreshtat = (int) Math.ceil((double) gjatesiaCipherText / gjatesiaKeyword);
+        char[][] matrica = new char[rreshtat][gjatesiaKeyword];
+
+        // Sort the key and store the sorted indices
+        char[] sortedKeyword = keywordCols.toCharArray();
+        Arrays.sort(sortedKeyword);
+        int[] sortedIndexes = new int[gjatesiaKeyword];
+        for (int i = 0; i < gjatesiaKeyword; i++) {
+            sortedIndexes[i] = keywordCols.indexOf(sortedKeyword[i]);
+        }
+
+        // Populate the grid with ciphertext using sorted key order
+        int index = 0;
+        for (int j = 0; j < gjatesiaKeyword; j++) {
+            int column = sortedIndexes[j];
+            for (int i = 0; i < rreshtat; i++) {
+                if (index < gjatesiaCipherText) {
+                    matrica[i][column] = cipherText.charAt(index++);
+                } else {
+                    matrica[i][column] = ' ';
+                }
+            }
+        }
+
+        // Create the plaintext from the grid
+        for (int i = 0; i < rreshtat; i++) {
+            for (int j = 0; j < gjatesiaKeyword; j++) {
+                decryptedText += matrica[i][j];
+            }
+        }
+
+        // Remove trailing spaces from the plaintext
+        return decryptedText.replaceAll("\\s+$", "");
+    }
